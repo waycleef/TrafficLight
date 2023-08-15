23721de06bd9a7ca751f12b76a8e6fbe0fb3dbaf
waycleef <khmalikov@gmail.com>
2023-08-15 17:50:53 +0600

    выполнение домашнего задания Светофор с помощью if-else

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var orangeLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var startButton: UIButton!
+    
+    override func viewWillLayoutSubviews() {
+        let radius = redLight.frame.width / 2
+        
+        redLight.layer.cornerRadius = radius
+        orangeLight.layer.cornerRadius = radius
+        greenLight.layer.cornerRadius = radius
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLight.alpha = 0.3
+        orangeLight.alpha = 0.3
+        greenLight.alpha = 0.3
+        
+        startButton.layer.cornerRadius = 10
     }
-
+    
+    @IBAction func startButtonTapped() {
+        startButton.setTitle("Next", for: .normal)
+        
+        if round(redLight.alpha * 10) / 10 == 0.3 && round(orangeLight.alpha * 10) / 10 == 0.3 && round(greenLight.alpha * 10) / 10 == 0.3 {
+            redLight.alpha = 1
+        } else if round(redLight.alpha * 10) / 10 == 1 && round(orangeLight.alpha * 10) / 10 == 0.3 && round(greenLight.alpha * 10) / 10 == 0.3 {
+            redLight.alpha = 0.3
+            orangeLight.alpha = 1
+        } else if round(redLight.alpha * 10) / 10 == 0.3 && round(orangeLight.alpha * 10) / 10 == 1 && round(greenLight.alpha * 10) / 10 == 0.3 {
+            orangeLight.alpha = 0.3
+            greenLight.alpha = 1
+        } else {
+            startButton.setTitle("Start", for: .normal)
+            greenLight.alpha = 0.3
+        }
+    }
+    
 
 }
 
